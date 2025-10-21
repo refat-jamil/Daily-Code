97d395813155f91958029244d17df70c28d48260
Refat Jamil <s.m.refat.jamil@gmail.com>
2025-10-21 11:04:33 +0600

    Day 1 Tuple

---
+++ day 1/tuple.py
@@ -0,0 +1,13 @@
+"""Tuples are used to store multiple items in a single variable.
+A tuple is a collection which is ordered and unchangeable.
+Tuples are written with round brackets."""
+
+
+courses = ('History', 'Math', 'Physics', 'ComSci')
+
+print(courses)
+
+courses[0] = 'Bangla' # unchangeable
+
+print(courses)
+
