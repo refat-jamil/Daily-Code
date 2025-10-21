366054ad9096edc22a6fd1c320532dee7671495f
Refat Jamil <s.m.refat.jamil@gmail.com>
2025-10-21 10:22:00 +0600

    Day 1 - Sorting, Finding, Looping, Splitting

--- day 1/list.py
+++ day 1/list.py
@@ -5,55 +5,81 @@
 
 courses = ['History', 'Math', 'Physics', 'ComSci']
 
-# print(courses)
-
-# print(len(courses))
-
-# print(courses[0])
-
-# print(courses[3])
-
-# print(courses[-1])
-
-# print(courses[0:3])
-
-# print(courses[2:])
-
-# courses.append('Art')
-
-# print(courses)
-
-# courses.insert(3, 'Chem')
-
-# print(courses)
+print(courses)
+print(len(courses))
+print(courses[0])
+print(courses[3])
+print(courses[-1])
+print(courses[0:3])
+print(courses[2:])
+courses.append('Art')
+print(courses)
+courses.insert(3, 'Chem')
+print(courses)
 
 courses_2 = ['ICT', 'Bio']
 
-# print(courses_2)
-
-# print(courses, courses_2)
-
-# courses.append(courses_2)
-
-# print(courses)
-
-# courses.insert(len(courses), courses_2)
-
-# print(courses)
-
+print(courses_2)
+print(courses, courses_2)
+courses.append(courses_2)
+print(courses)
+courses.insert(len(courses), courses_2)
+print(courses)
 courses.extend(courses_2)
-
 print(courses)
-
 courses.remove('Math')
-
 print(courses)
-
 courses.pop()
-
 print(courses)
-
 p = courses.pop()
-
 print(p)
 print(courses)
+
+
+courses = ['History', 'Math', 'Physics', 'ComSci']
+courses.reverse()
+
+
+nums = [1, 2, 3, 4, 5]
+nums.sort()
+print(nums)
+nums.sort(reverse=True)
+print(nums)
+
+
+
+nums = [100, 250, 370, 4, 50]
+sorted_list = sorted(nums)
+print(sorted_list)
+max_nums = max(nums)
+print(max_nums)
+sum_nums = sum(nums)
+print(sum_nums)
+
+
+
+
+courses = ['History', 'Math', 'Physics', 'ComSci']
+print(courses.index("Physics"))
+print('Physics' in courses)
+print('Chemistry' in courses)
+
+
+courses = ['History', 'Math', 'Physics', 'ComSci']
+for subject in courses:
+    print(subject)
+
+
+courses = ['History', 'Math', 'Physics', 'ComSci']
+for index, subject in enumerate(courses):
+    print(index, '-', subject)
+
+
+courses = ['History', 'Math', 'Physics', 'ComSci']
+course_str = ', '.join(courses)
+print(course_str)
+print(type(course_str))
+
+new_list = course_str.split(', ')
+print(course_str)
+print(new_list)
