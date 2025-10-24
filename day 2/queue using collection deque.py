813dc0430ac31eb5022d5f3de8de9e8c1d12b1bd
Refat Jamil <s.m.refat.jamil@gmail.com>
2025-10-24 23:59:38 +0600

    Day 2 : using collections.deque

---
+++ day 2/queue using collection deque.py
@@ -0,0 +1,17 @@
+from collections import deque
+q = deque()
+
+q.append(1)
+q.append(2)
+q.append(3)
+print(q)
+
+print(q.popleft())
+print(q)
+
+print(q.popleft())
+print(q)
+
+print(q.popleft())
+print(q)
+
