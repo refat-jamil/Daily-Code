cc8f3d136a5b02916a56e790c72397b78006fa80
Refat Jamil <s.m.refat.jamil@gmail.com>
2025-11-03 23:57:30 +0600

    Day 6: hashmap twoSum

---
+++ Day 6/twoSum.py
@@ -0,0 +1,9 @@
+def twoSum(nums, target):
+    hash_map = {}
+    for i, num in enumerate(nums):
+        complement = target - num
+        if complement in hash_map:
+            return [hash_map[complement], i]
+        hash_map[num] = i
+
+print(twoSum([2,7,11,15], 9))  
